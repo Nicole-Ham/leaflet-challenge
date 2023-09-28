8cfa2e9ba251648b5ba77b7578b014da965b01d2
Nicole-Ham <NLHammack@gmail.com>
2023-09-27 18:27:04 -0700

    update

--- Leaflet-Part-1/logic.js
+++ Leaflet-Part-1/logic.js
@@ -3,7 +3,23 @@ let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/signif
 
 // Perform a GET request to the query URL/
 d3.json(queryUrl).then(function (data) {
-    console.log(data);
     // Once we get a response, send the data.features object to the createFeatures function.
     createFeatures(data.features);
   });
+
+// function for features
+function createFeatures(earthquakeData) {
+
+    // Define a function that we want to run once for each feature in the features array.
+    // Give each feature a popup that describes the place and time of the earthquake.
+    function onEachFeature(feature, layer) {
+      layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
+    }
+};
+
+
+
+// function to create map
+function createMap(earthquakes) {};
+
+
